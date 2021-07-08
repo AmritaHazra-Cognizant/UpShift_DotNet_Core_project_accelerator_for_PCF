60ed82028764dbff616ffc374cccfe901cb2ea32
Amrita Hazra <Amrita.Hazra@Cognizant.com>
2021-07-08 20:31:25 +0530

    Updating config yml and promote build sh

--- .circleci/promote_build.sh
+++ .circleci/promote_build.sh
@@ -8,7 +8,7 @@ echo "--------Install JFrog cli installation is finished---------"
 
 
 ./jfrog rt c --url ${ARTIFACTORY_URL}artifactory --user $ARTIFACTORY_USER --apikey $ARTIFACTORY_TOKEN --interactive=false
-./jfrog rt s --props build.id=$CIRCLE_WORKFLOW_ID generic-dev-local/UpShift/dot-net-core-project-accelerator/ | jq .[-1].path -r > artifact_path.txt
+./jfrog rt s --props build.id=$CIRCLE_WORKFLOW_ID generic-dev-local/UpShift/Upshift_DotNet_Core_project_accelerator_for_PCF/ | jq .[-1].path -r > artifact_path.txt
 ARTIFACT_PATH=$(cat artifact_path.txt)
 ./jfrog rt cp $ARTIFACT_PATH generic-local/dot-net-core-project-accelerator_latest.zip
 ./jfrog rt cp $ARTIFACT_PATH generic-local/
